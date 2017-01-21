a13c7c54e370bc496f85172cc0021cc2ebd49813
web6600 <web6600@caelum55-04>
2017-01-20 22:02:49 -0200

    Exercicio 9.3 - Alterando classe AdicionaContato e criando Contato Adicionado - aula de MVC

--- fj21-agenda/src/br/com/caelum/servlet/AdicionaContatoServlet.java
+++ fj21-agenda/src/br/com/caelum/servlet/AdicionaContatoServlet.java
@@ -7,6 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -49,11 +50,14 @@ public class AdicionaContatoServlet extends HttpServlet {
 		ContatoDao dao = new ContatoDao();
 		dao.adiciona(contato);
 		
-		out.println("<html>");
-		out.println("<body>");
-		out.println("Contato " + contato.getNome() + " adicionado com sucesso");
-		out.println("</body>");
-		out.println("</html>");
+		RequestDispatcher rd = request.getRequestDispatcher("/contato-adicionado.jsp");
+		rd.forward(request, response);
+		
+//		out.println("<html>");
+//		out.println("<body>");
+//		out.println("Contato " + contato.getNome() + " adicionado com sucesso");
+//		out.println("</body>");
+//		out.println("</html>");
 	}
 	
 }
